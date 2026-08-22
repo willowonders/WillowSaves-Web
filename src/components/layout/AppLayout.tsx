ebedf93aad37325935bda3eac6060914e9a851a1
willowonders <juicewa129@gmail.com>
2026-08-22 08:03:17 +0800

    Fixed Header UI

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -14,7 +14,7 @@ export function AppLayout({ children }: AppLayoutProps) {
   return (
     <div className="min-h-screen bg-[#F2F3F5] dark:bg-canvas-dark flex">
       <Sidebar />
-      <div className="flex-1 flex flex-col min-h-screen lg:ml-60 min-w-0 overflow-hidden">
+      <div className="flex-1 flex flex-col min-h-screen lg:ml-60 min-w-0 lg:overflow-hidden">
         <Header />
         <DesktopTopBar />
         <main className="flex-1 p-4 sm:p-6 overflow-hidden min-w-0">
